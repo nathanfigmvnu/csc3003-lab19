bbbc0b878c81f6cb83bd887859ac44ae035300d7
Nathan Figueroa <figuernd@miamioh.edu>
2019-04-12 14:46:43 -0400

    Completed example

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <v-app>
+    <v-toolbar>
+      <v-toolbar-title>
+       Vue Check List
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-container>
+      <v-layout row justify-center>
+        <v-card class="ma-5" width="600">
+          <Item v-for="item in items" v-bind:text="item" />
+        </v-card>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import BlankComponent from './components/BlankComponent'
+import Item from './components/Item'
 
 export default {
   name: 'App',
   components: {
-    BlankComponent
+    Item
   },
   data () {
     return {
-      //
+      items: [
+        "Make pancakes",
+        "Get Amos insulin",
+        "Read a book",
+        "Take a nap"
+      ]
     }
   }
 }
---
+++ src/components/Item.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <i @click="checked=!checked" v-if="checked" class="material-icons">check_box</i>
+    <i @click="checked=!checked" v-if="!checked" class="material-icons">check_box_outline_blank</i>
+    <span>{{text}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Item",
+    data: () => ({
+      checked: false
+    }),
+    props: {
+      text: String
+    }
+  }
+</script>
+
+<style>
+i {
+  vertical-align: middle;
+  cursor: pointer;
+}
+</style>
